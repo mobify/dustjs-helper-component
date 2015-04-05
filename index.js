591b806a85a7740599ab134699152fb47b4d90f7
Ryan Frederick <ryan@ryanfrederick.com>
2015-04-05 09:33:58 -0700

    Fix AMD definitions.

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 (function(root, factory) {
-  if (typeof define === 'function' && define.amd) {
+  if (typeof define === 'function' && define.amd && define.amd.dust === true) {
     define([
-      'dustjs-linkedin',
       './lib/component-helper',
       './lib/component-extensions'
     ], factory);
   } else if (typeof exports === 'object') {
     module.exports = factory(
-      require('dustjs-linkedin'),
       require('./lib/component-helper'),
       require('./lib/component-extensions')
     );
