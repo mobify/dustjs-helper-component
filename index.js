cce4620e0d15fb65dcee87bb1d029ff9bed499a0
Ryan Frederick <ryan@ryanfrederick.com>
2015-04-20 20:29:32 -0700

    Downgrade to Dust 2.3.3

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 (function(root, factory) {
-  if (typeof define === 'function' && define.amd && define.amd.dust === true) {
+  if (typeof define === 'function' && define.amd) {
     define([
-      './lib/helpers',
       './lib/compiler'
     ], factory);
   } else if (typeof exports === 'object') {
     module.exports = factory(
-      require('./lib/helpers'),
       require('./lib/compiler')
     );
   } else {
